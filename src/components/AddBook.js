516eac19883077bb8ff45b2ee99ef8dc460df144
Vladimir Volkov <53047099+ApeIsHere@users.noreply.github.com>
2024-03-09 22:38:06 +0300

    Shorthand filtering

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -10,9 +10,7 @@ export default function AddBook({ books }) {
   const authors = [...new Set(books.map((book) => book.author))];
   const genres = [...new Set(books.map((book) => book.genre))];
 
-  function handleSubmit(e) {
-    e.preventDefault();
-
+  function filterBooks() {
     const filteredBooks = books.filter((book) => {
       const authorMatch = author === "" || book.author === author;
       const genreMatch = genre === "" || book.genre === genre;
@@ -28,6 +26,11 @@ export default function AddBook({ books }) {
     setShownBooks(filteredBooks);
   }
 
+  function handleSubmit(e) {
+    e.preventDefault();
+    filterBooks();
+  }
+
   return (
     <div className="block add">
       <h3>Add new book</h3>
@@ -35,7 +38,13 @@ export default function AddBook({ books }) {
         <div className="add__sort">
           <label>
             Author
-            <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+            <select
+              value={author}
+              onChange={(e) => {
+                setAuthor(e.target.value);
+                filterBooks();
+              }}
+            >
               <option value="">None</option>
               {authors.map((auth) => (
                 <option value={auth} key={auth}>
@@ -46,7 +55,13 @@ export default function AddBook({ books }) {
           </label>
           <label>
             Genre
-            <select value={genre} onChange={(e) => setGenre(e.target.value)}>
+            <select
+              value={genre}
+              onChange={(e) => {
+                setGenre(e.target.value);
+                filterBooks();
+              }}
+            >
               <option value="">None</option>
               {genres.map((gen) => (
                 <option value={gen} key={gen}>
@@ -57,7 +72,13 @@ export default function AddBook({ books }) {
           </label>
           <label>
             Length
-            <select value={length} onChange={(e) => setLength(e.target.value)}>
+            <select
+              value={length}
+              onChange={(e) => {
+                setLength(e.target.value);
+                filterBooks();
+              }}
+            >
               <option value="">None</option>
               <option value="short">Short</option>
               <option value="average">Average</option>
@@ -65,10 +86,10 @@ export default function AddBook({ books }) {
             </select>
           </label>
         </div>
-        <Button>Show</Button>
+        {author || genre || length || <Button>Show All</Button>}
         <div className="bookshelf">
           {shownBooks.map((book) => (
-            <Book book={book} btnText="Add" />
+            <Book book={book} btnText="Add" key={book.id} />
           ))}
         </div>
       </form>
